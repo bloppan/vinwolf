34f0f20eb9c15689c66671a4672ac8b5d0d0dd81
Bernardo Lopez <bernar792@gmail.com>
2025-08-11 10:27:48 +0200

    v0.6.7: WPs&WRs: Fix upper bundle size limit

--- constants/src/node.rs
+++ constants/src/node.rs
@@ -82,7 +82,7 @@ pub const REPORTED_WORK_REPLACEMENT_PERIOD: usize = 5;
 // The maximum size of is-authorized code in octets.
 pub const MAX_IS_AUTHORIZED_SIZE: usize = 64_000;
 // The maximum size of an encoded work-package together with its extrinsic data and import implications, in octets.
-pub const MAX_ENCODED_WORK_PACKAGE_SIZE: u64 = 12 * (1 << 20);
+pub const MAX_ENCODED_WORK_PACKAGE_SIZE: u64 = 13_794_305;
 // The number of erasure-coded pieces in a segment.
 pub const SEGMENT_PIECES: usize = 6;
 // The basic size of erasure-coded pieces in octets.
