64c9fb36b079dbfbc28a464906aa90ba9c6935a3
Bernardo Lopez <bernar792@gmail.com>
2025-12-22 23:33:39 +0100

    Bump vinwolf-target version to 0.3.8

--- vinwolf-target/src/fuzz.rs
+++ vinwolf-target/src/fuzz.rs
@@ -24,7 +24,7 @@ pub static VINWOLF_INFO: LazyLock<PeerInfo> = LazyLock::new(|| {
         app_version: Version {
             major: 0,
             minor: 3,
-            patch: 7,
+            patch: 8,
         },
         jam_version: Version {
             major: 0,
