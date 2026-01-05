053350bf0541570fcb59f3d1cbc44fdc4082d2ca
Bernardo Lopez <bernar792@gmail.com>
2026-01-05 14:48:38 +0100

    Create jam-utils crate and move codec into it

--- vinwolf-target/src/lib.rs
+++ vinwolf-target/src/lib.rs
@@ -80,9 +80,9 @@ pub fn read_all_bins(dir_path: &Path) -> Vec<(u32, PathBuf)> {
             let f = f.ok()?.path();
             if f.extension()? == "bin" {
                 let stem = f.file_stem()?.to_str()?;
-                if stem == "genesis" {
+                /*if stem == "genesis" {
                     return Some((0, f));
-                }
+                }*/
                 if let Ok(num) = stem.parse::<u32>() {
                     return Some((num, f));
                 }
