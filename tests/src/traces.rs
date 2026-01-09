c32343e76b476ad45b8464dbe6cede4d396a0809
Bernardo Lopez <bernar792@gmail.com>
2026-01-09 23:17:21 +0100

    0.7.2: Fix 1767889897_6429

--- tests/src/traces.rs
+++ tests/src/traces.rs
@@ -32,7 +32,7 @@ mod tests {
     // bien -> 1766244251_2939, 1766243861_2612, 
     // medio -> 1766244251_2939
     //const FUZZ_REPORT: &str = "/home/bernar/workspace/vinwolf/tests/jam-conformance/fuzz-reports/0.7.2/traces/1766243176";
-    const FUZZ_REPORT: &str = "/home/bernar/workspace/vinwolf/tests/jam-conformance/fuzz-reports/0.7.2/traces/1766243315_8065";
+    const FUZZ_REPORT: &str = "/home/bernar/workspace/vinwolf/tests/jam-conformance/fuzz-reports/0.7.2/traces/1767889897_6429/";
     
     #[test]
     fn run_single_fuzz_report() {
@@ -45,8 +45,8 @@ mod tests {
         let _ = process_all_bins(dir_base);
     }   
 
-    const TRACES_DIR: &str = "/home/bernar/workspace/vinwolf/tests/jamtestvectors/traces/";
-    //const TRACES_DIR: &str = "/home/bernar/workspace/vinwolf/tests/jam-conformance/fuzz-reports/0.7.2/traces/";
+    //const TRACES_DIR: &str = "/home/bernar/workspace/vinwolf/tests/jamtestvectors/traces/";
+    const TRACES_DIR: &str = "/home/bernar/workspace/vinwolf/tests/jam-conformance/fuzz-reports/0.7.2/traces/1767871405_6428/";
 
     #[test]
     fn run_all_traces_tests() {
